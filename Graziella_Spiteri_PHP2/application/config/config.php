2d3cdcb2474bfdfcd87bb3a09cb0e9698a60bdb9
grzspiteri <grzspiteri@live.com>
2017-06-16 19:13:18 +0200

    ok

--- Graziella_Spiteri_PHP2/application/config/config.php
+++ Graziella_Spiteri_PHP2/application/config/config.php
@@ -114,7 +114,7 @@ $config['enable_hooks'] = FALSE;
 | https://codeigniter.com/user_guide/general/creating_libraries.html
 |
 */
-$config['subclass_prefix'] = 'TW_';
+$config['subclass_prefix'] = 'SL_';
 
 /*
 |--------------------------------------------------------------------------
